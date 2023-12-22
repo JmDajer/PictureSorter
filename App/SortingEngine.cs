3a34c788ccca3b3300a35ddaccf560056062d0fe
ranton <ranton@ttu.ee>
2023-12-23 01:18:59 +0200

    Add Folder creation and image metadata

--- App/SortingEngine.cs
+++ App/SortingEngine.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +11,55 @@ namespace App
 {
     internal class SortingEngine
     {
+        private string ImportPath { get; set; }
+        private string ExportPath { get; set; }
 
+        public SortingEngine(string importPath, string exportPath)
+        {
+            ImportPath = importPath;
+            ExportPath = exportPath;
+        }
 
+        public void Sort()
+        {
+            
+        }
+
+        private void CreateFolder(string folderName)
+        {
+            var folderPath = ExportPath + Path.PathSeparator + folderName;
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+        }
+
+        private string GetImageDate(string imagePath)
+        {
+            string fileDate = "";
+
+            try
+            {
+                using (var image = Image.FromFile(imagePath))
+                {
+                    var propertyItems = image.PropertyItems;
+
+                    foreach (var propertyItem in propertyItems)
+                    {
+                        if (propertyItem.Id == 0x0132)
+                        {
+                            string dateString = Encoding.UTF8.GetString(propertyItem.Value);
+                            DateTime dateTime = DateTime.ParseExact(dateString, "yyyy:MM:d H:m:s", null);
+                            fileDate = dateTime.Date.ToString();
+                        }
+                    }
+                }
+            } catch (Exception e)
+            {
+                Console.Error.WriteLine(e.ToString());
+            }
+
+            return fileDate;
+        }
     }
 }
